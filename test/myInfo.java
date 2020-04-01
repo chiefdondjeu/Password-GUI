a14f664b4a5083e3658715df5cbe9913cdb9bfbc
chiefdondjeu <florentdondjeu@yahoo.com>
2020-04-01 05:27:03 -0400

    fixed repo layout

--- test/myInfo.java
+++
@@ -1,9 +0,0 @@
-public class myInfo 
-{
-	public static void main(String[] args)
-	{
-		System.out.println("Name: Florent Dondjeu Tschoufack");
-		System.out.println("email: fdondjeutschoufack@gulls.salisbury.edu");
-		System.out.println("Major: Computer Science");
-	}
-}
